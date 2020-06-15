445c7eb82ca5833131cfe131a1b953ddb8b42420
Diego Casanova <diegogcasanova@gmail.com>
2020-06-15 11:30:23 -0500

    Webapp displays map and the backend is the latest.  Things that have to be improve:
    1) Indicate birds at the different locations.
    2) Display the markers on the map. (the new code should provide the list to make the markers)
    3) Display the info in a user friendly manner.

--- flaskexample/views.py
+++ flaskexample/views.py
@@ -1,35 +1,30 @@
 import flask
 from flask import render_template, request, session, g, redirect, url_for, abort, render_template, flash
 from flaskexample import app
-from flaskexample.miniWorkingCode import set_cover_mine, analysis
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 import pandas as pd
 import psycopg2
+
+from flaskexample import Main
 
 app.config.from_object(__name__)
 
 @app.route('/')
 @app.route('/index')
 @app.route('/input')
-def addition_input():
+def input():
    return render_template("input.html")
 
-@app.route('/output')
-def addition_output():
-   lat = float(request.args.get('Lat'))
-   lng = float(request.args.get('Lng'))
-   distList = analysis(lat, lng)
-#   print(distList)
-   return render_template("output.html", sumofnums = distList, lat = lat, lon = lng)
+@app.route('/output', methods=['GET', 'POST'])
+def output():
+   address = request.args.get('Address')
 
-@app.route('/mapbox_js',methods=['GET','POST'])
-def mapbox_js():
-    lat = float(request.args.get('Lat'))
-    lng = float(request.args.get('Lng'))
-    return render_template(
-        "mapbox_js.html",
-        ACCESS_KEY=app.config['MAPBOX_ACCESS_KEY'],
-        lat = lat, lon = lng
-    )
-    
+   lat, lng = Main.give_lat_lgn(address)
+   dfAll = Main.produce_table()
+   
+   setMat, nTime, n_clusters_, dfCounts = Main.clustering_and_prob(dfAll)
+   outLoc, mapMarkerList, userOut = Main.output_generator(dfCounts, lat, lng, setMat, nTime, n_clusters_)
+   
+   return render_template("output.html", sumofnums = userOut, lat = lat, lon = lng)
+
